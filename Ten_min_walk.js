39f2de865e5c30f055e3188ca450a5918ba76b05
Ovy95 <jackoverton21@hotmail.com>
2020-04-23 12:33:25 +0100

    Test add for North vs South

--- Ten_min_walk.js
+++ Ten_min_walk.js
@@ -1,7 +1,11 @@
 const tenMinWalk = directions => {
+  
   if(directions.length !== 10) {
     return "Invalid walk"
-  }else{
+  }
+  if (directions.filter(x => x === 'n').length !== directions.filter(x => x === 's').length )
+  return "Ending up walking North for too long"
+   else {
     return directions
   }
 
--- Ten_min_walk.test.js
+++ Ten_min_walk.test.js
@@ -2,13 +2,18 @@ const tenMinWalk = require('./Ten_min_walk')
 
 describe('TenMinWalk', function (){
   it('Should have an array lengh equal to 10',() => {
-  const directions = ['n','n','n','n','n','n','n','n','n','n'];
-  expect(tenMinWalk(directions)).toEqual(['n','n','n','n','n','n','n','n','n','n'])
+  const directions = ['n','s','n','s','n','s','n','s','n','s'];
+  expect(tenMinWalk(directions)).toEqual(['n','s','n','s','n','s','n','s','n','s'])
   });
 
   it ('Returns a message INVAlID WALK if directions array is less or more then 10 characters long',() => {
-    const directions = ['n','n','n'];
+    const directions = ['n','e','w'];
     expect(tenMinWalk(directions)).toEqual("Invalid walk")
-  })
+  });
+
+  it ('Checks amount of Norths are in the array', () => {
+    const directions = ['n','n','n','n','n','n','n','n','n','s'];
+    expect(tenMinWalk(directions)).toEqual("Ending up walking North for too long")
+  });
 
 });
